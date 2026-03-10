52f5b8c53ab293249745870ddfc8577706b0586b
Victor Henriquez <henriquezv075@gmail.com>
2026-03-10 10:02:45 -0400

    Entrega final

--- scripts/gedates.js
+++ scripts/gedates.js
@@ -1,7 +1,5 @@
-// 1. Obtener el año actual para el copyright
 const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
-// 2. Obtener la fecha de última modificación del documento
 const lastMod = document.lastModified;
 document.getElementById("lastModified").textContent = `Last Modification: ${lastMod}`;
